a55a6c595ebc3c884d8922e0bd24e3eb7618726f
Felipe Rodrigues <feliperodrigues@gmail.com>
2015-12-26 02:52:47 -0200

    refactory code

--- hang_man.py
+++ hang_man.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import random
 
@@ -17,48 +18,83 @@ def clear():
   else:
     os.system('clear')
 
-def draw():
+def draw(bad_guesses, good_guesses, secret_word):
+  clear()
+  print('Strikes: {}/7'.format(len(bad_guesses)))
+  print('')
+
+  for letter in bad_guesses:
+    print(letter, end=' ')
+  print ('\n\n')
+
   for letter in secret_word:
     if letter in good_guesses:
       print(letter, end='')
     else:
       print('_', end='')
 
-  print('')
-  print('Strikes: {}/7'.format(len(bad_guesses)))
-  print('')
+def get_guess(bad_guesses, good_guesses):
+  while True:
+    guess = input('Choose a letter: ').lower()
+
+    if len(guess) != 1:
+      print('You can only guess a single letter')
+    elif guess in bad_guesses or guess in good_guesses:
+      print('You"ve already choose this letter. Try other')
+    elif not guess.isalpha():
+      print('You can only guess letters!')
+    else:
+      return guess
+
+def play(done):
+  clear()
+  secret_word = random.choice(words)
+  bad_guesses = []
+  good_guesses = []
+
+  while True:
+    draw(bad_guesses, good_guesses, secret_word)
+    guess = get_guess(bad_guesses, good_guesses)
+
+    if guess in secret_word:
+        good_guesses.append(guess)
+        found = True
+
+        for letter in secret_word:
+          if letter not in good_guesses:
+            found = False
+
+        if found:
+          print('You win!')
+          print('The secret word was {}'.format(secret_word))
+    else:
+      bad_guesses.append(guess)
+      if len(bad_guesses) == 7:
+        draw(bad_guesses, good_guesses, secret_word)
+        print('You lost')
+        print('The secret word was {}'.format(secret_word))
+        done = True
+
+    if done:
+      play_again = input('Play again? Y/n ')
+      if play_again.lower() != 'n':
+        return play(done = False)
+      else:
+        sys.exit()
+
+def welcome():
+  print('Welcome to Hang Man')
+  start = input("Press enter/return to start or Q to quit: ")
+
+  if start.lower != 'q':
+    return True
+  else:
+    print('Bye')
+    sys.exit()
+
+done = False
 
 while True:
-  start = input('Press enter/return to start, or enter Q to quit: ')
-  if start.lower() == 'q':
-    break
-  else:
-
-    secret_word = random.choice(words)
-    bad_guesses = []
-    good_guesses = []
-
-    while len(bad_guesses) < 5 and len(good_guesses) != len(list(secret_word)):
-      draw()
-
-      guess = input('Choose a letter: ').lower()
-
-      if len(guess) != 1:
-        print('You can only guess a single letter')
-        continue
-      elif guess in bad_guesses or guess in good_guesses:
-        print('You"ve already choose this letter. Try other')
-        continue
-      elif not guess.isalpha():
-        print('You can only guess letters!')
-        continue
-
-      if guess in secret_word:
-        good_guesses.append(guess)
-        if len(good_guesses) == len(list(secret_word)):
-          print('You win. The word was {}'.format(secret_word))
-          break
-      else:
-        bad_guesses.append(guess)
-    else:
-      print('You did not guess it. My secret word was {}'.format(secret_word))
+  clear()
+  welcome()
+  play(done)
